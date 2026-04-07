2f8627b8a4aeeb11246421e8a581b52fa3f64ee5
angeldzz <pintodiazangel@gmail.com>
2026-04-07 11:31:08 +0200

    Add project files.

---
+++ ApiOAuthEmpleados/Program.cs
@@ -0,0 +1,42 @@
+using ApiOAuthEmpleados.Data;
+using ApiOAuthEmpleados.Helpers;
+using ApiOAuthEmpleados.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Creamos una instancia de nuestro helper
+HelperActionOAuthService helper = new HelperActionOAuthService(builder.Configuration);
+//ESTA INSTANCIA SOLAMENTE DEBEMOS CREARLA UNA VEZ DENTRO DE NUESTRA APLICACION
+builder.Services.AddSingleton<HelperActionOAuthService>(helper);
+builder.Services.AddAuthentication(helper.GetAuthenticationSchema())
+    .AddJwtBearer(helper.GetJWTBearerOptions());
+
+// Add services to the container.
+string connectionString = builder.Configuration.GetConnectionString("SqlServer");
+builder.Services.AddDbContext<HospitalContext>(options => options.UseSqlServer(connectionString));
+builder.Services.AddTransient<RepositoryHospital>();
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapOpenApi();
+app.MapScalarApiReference();
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/scalar");
+    return Task.CompletedTask;
+});
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
