13cd657f3ad8b4f4b9068b344382847b3ad60786
angeldzz <pintodiazangel@gmail.com>
2026-04-07 13:44:58 +0200

    MVC prueba

---
+++ MvcOAuthApiEmpleados/Controllers/EmpleadosController.cs
@@ -0,0 +1,37 @@
+﻿using ApiOAuthEmpleados.Models;
+using Microsoft.AspNetCore.Mvc;
+using MvcOAuthApiEmpleados.Services;
+using System.Threading.Tasks;
+
+namespace MvcOAuthApiEmpleados.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private ServiceEmpleados service;
+        public EmpleadosController(ServiceEmpleados service)
+        {
+            this.service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Empleado> empleados = await this.service.GetEmpleadosAsync();
+            return View(empleados);
+        }
+        public async Task<IActionResult> Detalles(int id)
+        {
+            //TENEDREMOS EL TOKEN EN SESSION
+            string token = HttpContext.Session.GetString("TOKEN");
+            if (token == null)
+            {
+                ViewData["MENSAJE"] = "DEBES HACER EL LOGIN";
+                return View();
+            }
+            else
+            {
+                Empleado empleado = await this.service.FindEmpleadoAsync(id, token);
+                return View(empleado);
+            }
+                
+        }
+    }
+}
---
+++ MvcOAuthApiEmpleados/Services/ServiceEmpleados.cs
@@ -0,0 +1,97 @@
+﻿using ApiOAuthEmpleados.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
+
+namespace MvcOAuthApiEmpleados.Services
+{
+    public class ServiceEmpleados
+    {
+        private string UrlApi;
+        private MediaTypeWithQualityHeaderValue header;
+        public ServiceEmpleados(IConfiguration configuration)
+        {
+            this.UrlApi = configuration.GetValue<string>
+                ("ApiUrls:ApiEmpleados");
+            this.header = new MediaTypeWithQualityHeaderValue("application/json");
+        }
+        public async Task<string> LogInAsync(string user, string pass)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string request = "api/auth/login";
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                LoginModel model = new LoginModel
+                {
+                    UserName = user,
+                    Password = pass
+                };
+                string json = JsonConvert.SerializeObject(model);
+                StringContent content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync(request, content);
+                if (response.IsSuccessStatusCode == false)
+                {
+                    string data = await response.Content.ReadAsStringAsync();
+                    JObject keys = JObject.Parse(data);
+                    string token = keys.GetValue("response").ToString();
+                    return token;
+                }
+                else
+                {
+                    return "No se a podido obtener el token";
+                }
+            }
+        }
+        private async Task<T> CallApiAsync<T>(string request)
+        {
+            using (HttpClient client = new HttpClient()) 
+            { 
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                HttpResponseMessage response = await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+        //REALIZAMOS UNA SOBRECARGA DEL METODO
+        private async Task<T> CallApiAsync<T>(string request, string token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(this.UrlApi);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(this.header);
+                client.DefaultRequestHeaders.Add("Authorization", "bearer " + token );
+                HttpResponseMessage response = await client.GetAsync(request);
+                if (response.IsSuccessStatusCode)
+                {
+                    T data = await response.Content.ReadAsAsync<T>();
+                    return data;
+                }
+                else
+                {
+                    return default(T);
+                }
+            }
+        }
+        public async Task<List<Empleado>> GetEmpleadosAsync()
+        {
+            return await this.CallApiAsync<List<Empleado>>("api/empleados");
+        }
+        //POR AHORA RECIBIREMOS EL TOKEN E EL METODO
+        public async Task<Empleado> FindEmpleadoAsync(int id, string token)
+        {
+            return await this.CallApiAsync<Empleado>("api/empleados/" + id, token);
+        }
+    }
+}
